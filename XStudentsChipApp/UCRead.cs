fedc90dcc5b54c6e8b60d1d89af5522583d8af17
Stamat <xsouvd2@gmail.com>
2025-02-19 15:54:45 +0200

    add uc read

---
+++ XStudentsChipApp/UCRead.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XStudentsChipApp
+{
+    public partial class UCRead : UserControl
+    {
+        public UCRead()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
